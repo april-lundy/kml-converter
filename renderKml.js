f6e907fad6ee05d1bb6bd4da863b412f79ad9bf1
Chip Bell <chip.bell@spinen.com>
2015-04-14 20:10:50 -0400

    Making phantom async, so that we can run many more processes

--- renderKml.js
+++ renderKml.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
+var async = require('async');
 var xml2js = require('xml2js');
 var phantom = require('phantom');
 
-function snapShotPolygon(serialNumber, points) {
+function snapShotPolygon(serialNumber, points, complete) {
     phantom.create('--ssl-protocol=any', function(phantomHandle) {
         phantomHandle.createPage(function(page) {
             var url = 'http://localhost:1337#' + JSON.stringify(points);
@@ -11,6 +12,7 @@ function snapShotPolygon(serialNumber, points) {
                     console.log('Rendering ' + serialNumber);
                     page.render(serialNumber + '.png');
                     phantomHandle.exit();
+                    complete && complete();
                 }, 500);
             });
         });
@@ -29,10 +31,14 @@ function mapRawPointsToObjectArray(rawPointsString) {
 
 var fileContents = fs.readFileSync('example_small.kml', { encoding: 'utf8' });
 xml2js.parseString(fileContents, { trim: true }, function(err, result) {
-    result.kml.Document[0].Folder[0].Placemark.forEach(function(placemark) {
+    var placeMarkProcessors = result.kml.Document[0].Folder[0].Placemark.map(function(placemark) {
         var serialNumber = placemark.name[0];
         var rawPointsString = placemark.Polygon[0].outerBoundaryIs[0].LinearRing[0].coordinates[0];
         var points = mapRawPointsToObjectArray(rawPointsString);
-        snapShotPolygon(serialNumber, points);
+        return function(callback) {
+            snapShotPolygon(serialNumber, points, callback);
+        };
     });
+
+    async.series(placeMarkProcessors);
 });
