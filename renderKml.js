c0a9f2d244ce9f11c889a85cfdb5ad09b10eb82e
Chip Bell <chip.bell@spinen.com>
2015-09-26 20:35:41 -0400

    Cleaning up the way we parse

--- renderKml.js
+++ renderKml.js
@@ -27,8 +27,8 @@ function mapRawPointsToObjectArray(rawPointsString) {
   return rawPointsString.split(' ').map(function(token) {
     var splitTokens = token.split(',');
     return {
-      x: parseFloat(splitTokens[1]).toPrecision(8),
-      y: parseFloat(splitTokens[0]).toPrecision(8),
+      x: Number(splitTokens[1]).toPrecision(8),
+      y: Number(splitTokens[0]).toPrecision(8),
     };
   });
 }
