ee2d20bef0104378ae0902719f2153c17f2d0782
Chip Bell <chip.bell@spinen.com>
2015-09-27 19:56:17 -0400

    Adding a few docs

--- snapshot-polygon.js
+++ snapshot-polygon.js
@@ -1,6 +1,14 @@
 var page = require('webpage').create();
 var system = require('system');
 
+/**
+ * Simple script to snapshot a webpage. Currently, defaults to localhost:1337 pages:
+ *
+ * Usage:
+ *   phantomjs snapshot-polygon.js file.html output.png
+ *
+ * This call will snapshot localhost:1337/file.html and save it into output.png
+ */
 page.viewportSize = { width: 1024, height: 720 };
 page.open('http://localhost:1337/' + system.args[1], function(status) {
   window.setTimeout(function() {
