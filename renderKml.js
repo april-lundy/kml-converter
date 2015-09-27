a7f8dced9fbbd281db751f3c3c51bbaf90a0060b
Chip Bell <chip.bell@spinen.com>
2015-09-27 18:57:30 -0400

    Extracting polygons in a separate file

--- renderKml.js
+++ renderKml.js
@@ -1,5 +1,4 @@
-var fs = require('fs');
-var xml2js = require('xml2js');
+var extractPolygons = require('./extract-polygons');
 var phantom = require('phantom');
 var basename = require('path').basename;
 
@@ -24,36 +23,9 @@ function snapShotPolygons(serialNumber, polygons, complete) {
   });
 };
 
-function mapRawPointsToObjectArray(rawPointsString) {
-  return rawPointsString.split(' ').map(function(token) {
-    var splitTokens = token.split(',');
-    return {
-      x: Number(Number(splitTokens[1]).toPrecision(8)),
-      y: Number(Number(splitTokens[0]).toPrecision(8)),
-    };
-  });
-}
-
-function tmeToOpacity(tme) {
-  if(isNaN(Number(tme))) {
-    console.log('  BAD TME:', tme);
-    return 0.5;
-  }
-
-  return (5 - Number(tme)) / 4;
-}
 
 var filename = process.argv[2];
-var fileContents = fs.readFileSync(filename, { encoding: 'utf8' });
-xml2js.parseString(fileContents, { trim: true }, function(err, result) {
-  var polygons = result.kml.Document[0].Folder[0].Placemark.map(function(placemark) {
-    var rawPointsString = placemark.Polygon[0].outerBoundaryIs[0].LinearRing[0].coordinates[0];
-    return {
-      points: mapRawPointsToObjectArray(rawPointsString),
-      opacity: tmeToOpacity(placemark.name),
-    };
-  });
-
+extractPolygons(filename, function(polygons) {
   console.log('About to load', basename(filename, '.kml'));
   snapShotPolygons(basename(filename, '.kml'), polygons);
 });
