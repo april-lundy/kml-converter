b2a8d5b29598a8d70e4f188c58449ce8a3fa66d2
Chip Bell <chip.bell@spinen.com>
2015-09-27 20:12:02 -0400

    More comments

--- renderKml.js
+++ renderKml.js
@@ -6,13 +6,18 @@ var phantom = require('phantomjs').path;
 var childProcess = require('child_process');
 
 var snapShotPolygons = function(name, polygons) {
+  // Use index.tmpl.html as a template, but substitute in the polygons provided
   templateFile('index.tmpl.html', name + '.html', { polygons: polygons });
+
+  // build out the args to the phantom script and run it:
+  // phantomjs snapshot-polygon.js name.html name.png
   var args = [
     path.join(__dirname, 'snapshot-polygon.js'),
     name + '.html',
     name + '.png'
   ];
   childProcess.execFile(phantom, args, function(err, stdout, stderr) {
+    // Once we've finished, print out any forwarded output
     if(err) {
       console.log(err);
     }
@@ -24,6 +29,7 @@ var snapShotPolygons = function(name, polygons) {
   });
 };
 
+// use the second arg (node renderKml.js FILENAME.kml) as the input file
 var filename = process.argv[2];
 extractPolygons(filename, function(polygons) {
   console.log('About to load', path.basename(filename, '.kml'));
