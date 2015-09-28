64c2ede935f6ca2cb649df08e02afc2a8ee54c8e
Chip Bell <chip.bell@spinen.com>
2015-09-27 20:08:55 -0400

    Improving the snapshot process

--- renderKml.js
+++ renderKml.js
@@ -1,31 +1,27 @@
+var path = require('path');
 var extractPolygons = require('./extract-polygons');
-var phantom = require('phantom');
-var basename = require('path').basename;
+var templateFile = require('./template-file')
+var phantom = require('phantomjs').path;
+var childProcess = require('child_process');
 
-function snapShotPolygons(serialNumber, polygons, complete) {
-  phantom.create('--ssl-protocol=any', function(phantomHandle) {
-    phantomHandle.createPage(function(page) {
-      var url = 'http://localhost:1337#' + JSON.stringify(polygons);
-      console.log(url.length);
-      page.open(url, function(status) {
-        setTimeout(function() {
-          page.evaluate(function() {
-            document.querySelector('.leaflet-control-attribution').style.visibility = 'hidden';
-          });
-          console.log('  Rendering');
-          page.render(serialNumber + '.png');
-          phantomHandle.exit();
-          complete && complete();
-          console.log('  Done.');
-        }, 500);
-      });
-    });
+var snapShotPolygons = function(name, polygons) {
+  templateFile('index.tmpl.html', name + '.html', { polygons: polygons });
+  var args = [
+    path.join(__dirname, 'snapshot-polygon.js'),
+    name + '.html',
+    name + '.png'
+  ];
+  childProcess.execFile(phantom, args, function(err, stdout, stderr) {
+    if(err) {
+      console.log(err);
+    }
+    console.log(stdout);
+    console.log(stderr);
   });
 };
 
-
 var filename = process.argv[2];
 extractPolygons(filename, function(polygons) {
-  console.log('About to load', basename(filename, '.kml'));
-  snapShotPolygons(basename(filename, '.kml'), polygons);
+  console.log('About to load', path.basename(filename, '.kml'));
+  snapShotPolygons(path.basename(filename, '.kml'), polygons);
 });
