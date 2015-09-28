3f51b1bb9b8ef7e59c80baaac595d622e1d044bf
Chip Bell <chip.bell@spinen.com>
2015-09-27 20:13:44 -0400

    More comments

--- template-file.js
+++ template-file.js
@@ -1,6 +1,15 @@
 var fs = require('fs');
 var _ = require('lodash');
 
+/**
+ * Loads a template and renders it to an output file with the provided data
+ *
+ * @param templateFile The path to the template
+ * @param outputFile   The file to put the output in
+ * @param data         The data to pass to the template
+ *
+ * @return void
+ */
 module.exports = function(templateFile, outputFile, data) {
   var contents = fs.readFileSync(templateFile);
   var templateFunction = _.template(contents);
